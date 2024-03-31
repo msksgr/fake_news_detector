c8f755438a228d43aa568408d9c517adecd9193c
msksgr <misaki.sagara@student.ie.edu>
2024-03-31 15:01:21 +0100

    first commit

---
+++ Fake_News_Detector.py
@@ -0,0 +1,183 @@
+# Import libraries
+import streamlit as st
+from PIL import Image
+import pandas as pd
+import base64
+
+# Set page config
+st.set_page_config(
+    page_title="Fake News Detector",
+    page_icon="🤖",
+    layout="wide",
+    initial_sidebar_state="expanded"
+)
+
+# URLs for the app and GitHub repository
+app_url = 'https://share.streamlit.io/your-github-username/your-repo-name/main/Fake_News_App.py'
+github_url = 'https://github.com/Jeisson-rojas/2_Steps_Fake_News_Detector' 
+
+# Set background image
+## Function to get image files as base64 (binary data)
+@st.cache_data
+def get_img_as_base64(file):
+    with open(file, 'rb') as f:
+        data = f.read()
+    return base64.b64encode(data).decode()
+
+## Get image files as base64 (binary data) to display in the app
+img_bg = get_img_as_base64("C:/Users/msksb/OneDrive - IE University/Documentos/VS/nlp_app/data/background.png")
+img_sidebar = get_img_as_base64("C:/Users/msksb/OneDrive - IE University/Documentos/VS/nlp_app/data/image.jpg")
+
+## Settings for the background image using CSS
+page_bg_img = f"""
+<style>
+[data-testid="stAppViewContainer"] > .main {{
+background-image: url("data:image/png;base64,{img_bg}");
+background-size: 100%;
+}}
+[data-testid="stHeader"] {{
+background: rgba(0,0,0,0);
+}}
+[data-testid="stToolbar"] {{
+right: 2rem;
+}}
+</style>
+"""
+
+## CSS code for the background image (optional, needs to be in the above css code)
+# background-position: top left;
+# background-repeat: no-repeat;
+# background-attachment: local;
+
+## Sidebar photo settings (optional, needs to be in the above css code)
+# [data-testid="stSidebar"] > div:first-child {{
+# background-image: url("data:image/png;base64,{img_sidebar}");
+# background-position: center; 
+# background-repeat: no-repeat;
+# background-attachment: fixed;
+# }}
+
+## Display the background image
+st.write(page_bg_img, unsafe_allow_html=True)
+
+
+# Load news data
+## News data
+df_news = pd.read_excel('C:/Users/msksb/OneDrive - IE University/Documentos/VS/nlp_app/data/news_poc.xlsx')
+
+## Define a function to extract opening sentences from news text
+def extract_first_few_words(text, num_words):
+    words = text.split()
+    first_few_words = ' '.join(words[:num_words])
+    return first_few_words
+
+## Extract opening sentences from news articles
+num_words = 30 # Number of words to extract
+df_news['Opening'] = df_news['Text'].apply(lambda x: extract_first_few_words(x, num_words))
+
+## Close words in the similar news
+pr_excel = pd.read_excel('C:/Users/msksb/OneDrive - IE University/Documentos/VS/nlp_app/data/pagerank.xlsx', sheet_name=None)
+df_pr_real_news_1 = pr_excel['real_news_1']
+df_pr_real_news_2 = pr_excel['real_news_2']
+df_pr_fake_news_1 = pr_excel['fake_news_1']
+df_pr_fake_news_2 = pr_excel['fake_news_2']
+
+# Load images
+## Gauge icons for the probability of the news being fake 
+img_pt1 = Image.open('C:/Users/msksb/OneDrive - IE University/Documentos/VS/nlp_app/data/pt1.png')
+img_pt2 = Image.open('C:/Users/msksb/OneDrive - IE University/Documentos/VS/nlp_app/data/pt2.png')
+img_pt3 = Image.open('C:/Users/msksb/OneDrive - IE University/Documentos/VS/nlp_app/data/pt3.png')
+img_pt4 = Image.open('C:/Users/msksb/OneDrive - IE University/Documentos/VS/nlp_app/data/pt4.png')
+
+## Word cloud images for each news
+img_wc_real_news_1 = Image.open('C:/Users/msksb/OneDrive - IE University/Documentos/VS/nlp_app/data/wc_real_news_1.jpg')
+img_wc_real_news_2 = Image.open('C:/Users/msksb/OneDrive - IE University/Documentos/VS/nlp_app/data/wc_real_news_2.jpg')
+img_wc_fake_news_1 = Image.open('C:/Users/msksb/OneDrive - IE University/Documentos/VS/nlp_app/data/wc_fake_news_1.jpg')
+img_wc_fake_news_2 = Image.open('C:/Users/msksb/OneDrive - IE University/Documentos/VS/nlp_app/data/wc_fake_news_2.jpg')
+
+
+
+# Main page
+
+# Title and description
+st.title('Fake News Detector')
+st.header('Welcome to our fake news detector!')
+
+st.container()
+col1, col2 = st.columns([2, 1])
+
+with col1:
+    st.subheader(
+        'Not sure if a news article is real or fake? Our tool helps you determine the likelihood of news being fake.'
+        )
+    'This application is designed to help you distinguish between real and fake news articles. In today\'s digital age, where misinformation is rampant, it\'s essential to verify the authenticity of news sources. Our tool aims to assist you in making informed decisions by providing insights into the likelihood of news articles being fake.'
+    'This prototype of our fake news detector is built with the upcoming US presidential election in mind. We understand the importance of identifying fake news and avoiding misinformation, especially during crucial events like elections.'
+    'For detailed instructions on using the application, you can access the manual below or from the sidebar navigation.'
+    st.page_link('pages/Manual.py', label='Manual', icon='📖')
+ 
+    st.divider()
+
+    # Step 1: Select a news
+    st.header("Step 1: Select a News Article")
+    'Choose a news article title to analyze its likelihood of being fake.'
+
+    # Select a news title
+    selected_title = st.selectbox('Select a news title:', df_news['Title'], label_visibility='collapsed')
+
+    # Retrieve selected news data
+    selected_news = df_news[df_news['Title'] == selected_title].iloc[0]
+    opening_text = selected_news['Opening']
+    news_num = selected_news['No.']
+    probability = selected_news['Fake Probability'] * 100
+
+    # Display the selected news
+    if not selected_news.empty:  
+        'You selected the news beginning with:'
+        container = st.container(border=True)
+        container.write(opening_text + '...')
+    else:
+        container = st.container(border=True)
+        container.write('No news selected.')
+    
+    'Click the button below to analyze the news article.'
+
+    # After submitted, show every data related to the selected news
+    if st.button('Submit'):
+        
+        # Step 2: Display the result
+        st.header('Step 2: Check the Result')
+
+        col1, col2 = st.columns([1, 2])
+
+        with col1:
+            'The news is'
+            st.subheader(f'{probability:.2f}%')
+            'likely to be fake.'
+
+        with col2:      
+            # Gauge icon depending on the probability
+            if probability < 25:
+                st.image(img_pt1, width=250)
+            elif probability < 50:
+                st.image(img_pt2, width=250)
+            elif probability < 75:
+                st.image(img_pt3, width=250)
+            else:
+                st.image(img_pt4, width=250)
+
+        # Step 3: Display word cloud
+        st.header('Step 3: Word Cloud of the Selected News')
+        'The word cloud shows the most frequent words in the selected news article. The size of the word indicates the frequency.'
+        st.image(eval(f'img_wc_{news_num}'), width=600)
+
+        # Step 4: Display related words
+        st.header('Step 4: Related words in similar news')
+        'Reviewing related words in similar news articles can help understand their context. News articles are categorized into two groups: real and fake, based on the probability of being fake.'
+        'The table below shows the most related words in each news group, along with their importance.'
+
+        st.dataframe(eval(f'df_pr_{news_num}'), hide_index=True)
+
+        # Closing words
+        'Thank you for using our Fake News Detector! We hope you find this tool helpful.'
+        'For more details about the model, visit our GitHub page:'
+        st.page_link(github_url, label='GitHub')
---
+++ pages/Manual.py
@@ -0,0 +1,56 @@
+import streamlit as st
+
+# Set page config
+st.set_page_config(
+    page_title="Fake News Detector",
+    page_icon="🤖",
+    # layout="wide",
+    initial_sidebar_state="expanded"
+)
+
+
+st.title('Fake News Detector User Manual')
+
+# Make sure to update to the latest URL
+app_url = 'https://share.streamlit.io/your-github-username/your-repo-name/main/Fake_News_App.py'
+github_url = 'https://github.com/Jeisson-rojas/2_Steps_Fake_News_Detector' 
+
+# Description about the application
+st.header('Introduction')
+'Welcome to the Fake News Detector! This application is designed to help you distinguish between real and fake news articles. In today\'s digital age, where misinformation is rampant, it\'s essential to verify the authenticity of news sources. Our tool aims to assist you in making informed decisions by providing insights into the likelihood of news articles being fake.'
+'This prototype of our fake news detector is built with the upcoming US presidential election in mind. We understand the importance of identifying fake news and avoiding misinformation, especially during crucial events like elections. Our machine learning model, based on LSTM neural networks trained with a dataset of fake and real news, provides accurate predictions of news authenticity. For more details about our model, please visit our GitHub repository.'
+st.page_link(github_url, label='GitHub Repository', icon='🔗')
+
+# Description about the manual
+st.header('Accessing the application')
+'To begin using the Fake News Detector:'
+'* Open the application link provided below in your web browser:'
+st.page_link(app_url, label='Fake News Detector', icon='🔗')
+'* The application is compatible with the two most recent versions of the following browsers: Google Chrome, Mozilla Firefox, Microsoft Edge, and Safari.'   # Reference: https://docs.streamlit.io/knowledge-base/using-streamlit/supported-browsers
+'* Once you\'ve accessed the application, you\'ll be presented with the main interface, where you can interact with features and functionalities.'
+
+st.header('User Interface Overview')
+'The Fake News Detector interface consists of several components:'
+'* **Title and Description**: Provides an overview of the application\'s purpose and functionality.'
+'* **Sidebar Navigation**: Allows you to directly access the main page and the manual.'
+'* **News Selection**: Enables you to select a news article from a list to analyze its authenticity.'
+'* **Result Display**: Presents the probability of the selected news being fake, along with related words and a word cloud visualization.'
+
+st.header('Using the Application')
+st.subheader('1. Selecting a News Article:')
+'* Choose a news article from the dropdown list in the "Step 1: Select a news" section.'
+'* The application will display the opening sentences of the selected news article.'
+st.subheader('2. Analyzing the News:')
+'* Click the "Submit" button to analyze the selected news article.'
+'* The application will provide a probability percentage indicating the likelihood of the news being fake.'
+'* Additionally, you\'ll see a gauge icon representing the probability level.'
+st.subheader('3. Viewing Word Cloud:')
+'* The application generates a word cloud visualization of the selected news article.' 
+'* This visualization highlights the most frequent words in the news article, with word size indicating frequency.'
+st.subheader('4. Exploring Related Words:')
+'* After submitting, the application will display most related words in similar news articles, categorized into real and fake news groups based on the probability of the news being fake.'
+'* These words are listed based on their importance in the news group.'
+'* This will help you to understand the overall context of the similar news.'
+
+
+'**Thank you for using the Fake News Detector! We hope this tool helps you navigate the vast landscape of online information more effectively.**'
